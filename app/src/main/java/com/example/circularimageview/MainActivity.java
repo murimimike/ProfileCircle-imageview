0ddf603fdadaaee085bf6ba33aa5a05b8b8d6827
murimimike <50362713+murimimike@users.noreply.github.com>
2020-04-17 21:26:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/circularimageview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.circularimageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
